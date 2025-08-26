3bcca3f8bed9aa5891f5f80dbde3288dd7a5aeeb
unknown <thduszbzb12@gmail.com>
2025-08-26 16:45:19 +0900

    APP,HEADER CSS 구현

--- src/App.jsx
+++ src/App.jsx
@@ -12,32 +12,35 @@ import UnnamedDetail from './Boardcomponents/UnnamedDetail';
 import PostDetail from './Boardcomponents/PostDetail';
 
 function App() {
-  const [test,setTest] = useState();
+  const [test, setTest] = useState();
 
   useEffect(() => {
     axiosInstance.get("/test")
-     .then(response => {
-      setTest(response.data)
-     }).catch(error => {
-      console.log(error)
-     })
-  },[])
+      .then(response => {
+        setTest(response.data)
+      }).catch(error => {
+        console.log(error)
+      })
+  }, [])
 
   return (
-    <>
-      <h1>{test}</h1> 
+    <div className="app-bg">
       <Header />
-
-      <Routes>
-        <Route path='/' element={<MainPage />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/unnamed' element={<Unnamed />} />
-        <Route path='/post' element={<Post />} />
-        <Route path='/unnamed/:id' element={<UnnamedDetail />} />
-        <Route path='/post/:id' element={<PostDetail />} />
-      </Routes>
-    </>
+      <div className="app-container">
+        <div className="app-card">
+          <h1 className="app-title">{test }</h1>
+          <Routes>
+            <Route path='/' element={<MainPage />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<Signup />} />
+            <Route path='/unnamed' element={<Unnamed />} />
+            <Route path='/post' element={<Post />} />
+            <Route path='/unnamed/:id' element={<UnnamedDetail />} />
+            <Route path='/post/:id' element={<PostDetail />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
   )
 }
 
