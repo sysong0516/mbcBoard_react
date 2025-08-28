c31b132315502f2d584714aaa418807502776ca9
sysong0516 <sysong777@naver.com>
2025-08-28 12:03:54 +0900

    토큰관련 작업중

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,8 +1,9 @@
 import axios from "axios";
-
+const token = sessionStorage.getItem("jwt");
 const axiosInstance = axios.create({
   baseURL:`${import.meta.env.VITE_SERVER_URL}`,
   headers: {
+    'Authorization': token,
     'Content-Type':'application/json; charset=utf-8'
   }
 });
