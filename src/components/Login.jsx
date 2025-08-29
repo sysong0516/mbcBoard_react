2ed40a53f5370a93517d311888549541cf621d92
sysong0516 <sysong777@naver.com>
2025-08-29 16:52:17 +0900

    로그인 정보 저장(username)

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './Login.css'
 import axiosInstance from "../axiosInstance";
 import UseNavi from "../UseNavi";
 
 const Login = ({ setAuth }) => {
-  const [member, setMember] = useState({
-    username: "",
-    password: "",
-    email: ""
-  });
-  const { goIndex } = UseNavi();
 
-  const onChangeHandler = (e) => {
-    setMember({
-      ...member,
-      [e.target.name]: e.target.value
-    });
+  const { goIndex } = UseNavi();
+  
+  const [rememberMe, setRememberMe] = useState(localStorage.getItem("savedId")?true:false);
+
+  const toogleSaveId = (e) => {
+    const id = document.querySelector('input[name="username"]').value;
+    if (e.target.checked) {
+      setRememberMe(true);
+    }
+    else {
+      setRememberMe(false);      
+    }
   };
+
+  useEffect(() => {
+    const savedUsername = localStorage.getItem("savedId");
+    if (savedUsername) {
+      document.querySelector('input[name="username"]').value = savedUsername;
+    }
+  }, []);
 
   return (
     <div className="login-container">
@@ -28,20 +36,25 @@ const Login = ({ setAuth }) => {
         <form>
           <div className="input-group">
             <i className="fas fa-envelope"></i>
-            <input type="text" placeholder="Email or ID" name="username" onChange={onChangeHandler} required autoComplete="off"/>
+            <input type="text" placeholder="ID" name="username" required autoComplete="off" />
           </div>
           <div className="input-group">
             <i className="fas fa-lock"></i>
-            <input type="password" placeholder="Password" name="password" onChange={onChangeHandler} required autoComplete="off" />
+            <input type="password" placeholder="Password" name="password"  required autoComplete="off" />
           </div>
           <div className="options">
             <label>
-              <input type="checkbox" /> Remember me
+              <input type="checkbox" checked={rememberMe} onChange={toogleSaveId} /> Remember me
             </label>
 
           </div>
           <button type="submit" className="login-btn" onClick={(e) => {
             e.preventDefault();
+            const member={
+              username: document.querySelector('input[name="username"]').value,
+              password: document.querySelector('input[name="password"]').value
+            };
+            console.log(member);
             axiosInstance.post("/login", member)
               .then(response => {
                 const jwt = response.headers.authorization; // response에서 jwt 토큰을 가져옴
@@ -50,6 +63,12 @@ const Login = ({ setAuth }) => {
                   sessionStorage.setItem('jwt', jwt); // sessionStorage에 jwt 토큰 저장
                   setAuth(true)
                   alert("로그인 성공");
+                  if (rememberMe) {
+                    localStorage.setItem("savedId", member.username);
+                  }
+                  else {
+                    localStorage.removeItem("savedId");
+                  }
                   goIndex();
                 }
               }).catch(error => {
