b6bf04aef683737d685b36815c5d8d369d03ba78
unknown <thduszbzb12@gmail.com>
2025-09-01 15:37:14 +0900

    삭제 구현 완료

--- src/message-components/ReceivedMessage.jsx
+++ src/message-components/ReceivedMessage.jsx
@@ -5,14 +5,14 @@ import { data } from "@remix-run/router";
 const ReceivedMessage = () => {
   const [messages, setMessages] = useState([]); // 메세지들이라서 배열 형태로
   const [show,setShow] = useState(false); // 체크박스 보이게 안 보이게
-  const [selet,setSelet] = useState([]); // 체크박스 선택 한 것들 값을 저장
+  const [select,setSelect] = useState([]); // 체크박스 선택 한 것들 값을 저장
 
   useEffect(() => {
     // 받은 메세지 불러오기
     axiosInstance.get('/messages/received')
     .then(responsse => {
       setMessages(responsse.data);
-      console.log(responsse.data)
+      
     })
     .catch(error => {
       console.error(error);
@@ -23,31 +23,41 @@ const ReceivedMessage = () => {
     setShow(true);
   };
 
-//   const seletHandle = (id) => {
-//       setSelet(prev => {
-//         return prev.includes(id) ? 
-//         prev.filter(i => i !== id)
-//         :
-//         [...prev,id] 
-//       })        
-//   };
-//  console.log(id)
+  const seletHandle = (id) => {
+      setSelect(prev => {
+        return prev.includes(id) ? 
+        prev.filter(i => i !== id)
+        :
+        [...prev,id] 
+      })        
+  };
 
   const deleteHandle = () => {
+    
+    axiosInstance.delete('/messages/delete/receiver',{
+      data : select
+    })
+    .then(responsse => {
+    alert('삭제 완료');
 
+    window.location.reload();
+    }).catch(error => {
+      console.error(error);
+      alert('삭제 실패');
+    })
   };
 
   return(
     <div>
 
       <h2>받은 메세지</h2>
-      <button onClick={showHandle}>삭제</button>
+      <button onClick={show ? deleteHandle : showHandle}>삭제</button>
 
       <ul>
         {messages.map( (msg,i)=>{
           return(
             <li key={msg.id} >
-              {show ? <input type="checkbox" onChange={seletHandle}/> : <></> }<p>보낸 사람 : {msg.sender.username}<br/>내용 : {msg.content}</p>
+              {show ? <input type="checkbox" onChange={()=>seletHandle(msg.id)}/> : <></> }<p>보낸 사람 : {msg.sender.username}<br/>내용 : {msg.content}</p>
             </li>
           );
         })}
--- src/message-components/SentMessage.jsx
+++ src/message-components/SentMessage.jsx
@@ -4,6 +4,8 @@ import axiosInstance from "../axiosInstance";
 
 const SentMessage = () => {
   const [messages, setMessages] = useState([]);
+  const [show, setShow] = useState(false);
+  const [select, setSelect] = useState([]);
 
   useEffect(() => {
     // 보낸 메세지 불러오기
@@ -17,17 +19,48 @@ const SentMessage = () => {
     })
   },[]);
 
+  const showHandle = () =>{
+    setShow(true);
+  };
+
+  const selectHandle = (id) => {
+    setSelect(prev => {
+      return prev.includes(id) ? 
+      prev.filter(i => i !== id)
+      :
+      [...prev,id]
+    })
+  };
+
+  const deleteHandle = () => {
+    axiosInstance.delete('/messages/delete/sender',{
+      data : select
+    })
+    .then(response => {
+      alert('삭제 완료 ');
+
+      window.location.reload();
+    }).catch(error => {
+      console.error(error);
+      alert('삭제 실패');
+    })
+  };
+
   return(
    <div>
 
       <h2>보낸 메세지</h2>
+      <button onClick={show ? deleteHandle : showHandle}>삭제</button>
 
       <ul>
-        {messages.map(msg => (
-          <li key={msg.id}><p>받는 사람 : {msg.receiverName}<br/>내용 : {msg.content}</p></li>
-        ))}
-        
-      
+        {messages.map( (msg,i) => {
+          return(
+             <li key={msg.id}>
+            {show ? <input type="checkbox" onChange={()=>selectHandle(msg.id)}/> : <></>}<p>받는 사람 : {msg.receiverName}<br/>내용 : {msg.content}</p>
+            </li>
+          );
+        })}
+
       </ul>
 
     </div>
