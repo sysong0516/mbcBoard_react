b9030200cad39fb31250851446c3fbab943c09a2
bae-kiwoong <baekw85@gmail.com>
2025-09-02 14:25:41 +0900

    메인 게시판 CSS , 룰렛 중복불가

--- src/maincomponents/UnamedPost.jsx
+++ src/maincomponents/UnamedPost.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import "./UnamedPost.css";
 import UseNavi from "../UseNavi";
 import axiosInstance from "../axiosInstance";
 import { Link } from "react-router-dom";
@@ -20,14 +21,16 @@ const UnamedPost = ({url,title}) => {
   return(
     <div>
       <div className="unnamed">
-        <h2>{title}</h2>
-        {
-          url === '/unnamed' || url === '/post' ?
-          <button onClick={() => {
-            goTo(url)
-          }}>더보기&gt;</button>
-          : ""
-        }
+        <div className="unnamed-header">
+          <h2>{title}</h2>
+          {
+            url === '/unnamed' || url === '/post' ?
+            <button onClick={() => {
+              goTo(url)
+            }}>더보기&gt;</button>
+            : ""
+          }
+        </div>
         <table>
           <thead>
             <tr>
